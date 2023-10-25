be2ebeaf2a5e5fc40bca552d75c4309ad447bc6c
SekolsKitten <144355273+SekolsKitten@users.noreply.github.com>
2023-10-25 07:57:26 -0400

    Create mousemover.py

---
+++ mousemover.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+running = True
+while running:
+    pyautogui.moveTo(763, 733)
+    pyautogui.moveTo(770, 950)
+    pyautogui.moveTo(860, 220)
+    pyautogui.moveTo(2689, 733)
+    pyautogui.moveTo(2696, 955)
+    pyautogui.moveTo(2776, 216)
